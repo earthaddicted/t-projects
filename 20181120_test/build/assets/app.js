45c2ba16474eda5d8241ae44a029e78cdbfaca38
earthaddicted <elenika.om@gmail.com>
2018-11-22 23:38:53 +0200

    fix sidebar arrows on mobile breakpoint(812)

--- 20181120_test/build/assets/app.js
+++ 20181120_test/build/assets/app.js
@@ -1,59 +1,25 @@
-// function themeSwitcher(btnId) {
-// 	if (btnId == "#my_btn-font") {
-// 		var isBig = $("#my_btn-font").text() == "A/a";
-// 		var newTitle = isBig ? 'a/A' : 'A/a';
-// 		$("#my_btn-font").html(newTitle);
-// 	} else if (btnId == "#my_btn-bg") {
-// 		var isLight = $("#my_btn-bg").text() != "Dark Theme";
-// 		// var newTitle = isLight ? 'Dark Theme' : 'Light Theme';
 
-// 		var isLight = $("#my_btn-bg").text() != "Dark Theme";
-// 		var newTitle = isLight ? 'Dark Theme' : 'Light Theme'
-// 		$("#my_btn-bg").html(newTitle);
-// 	} else {
-// 		throw "unexpected Id";
-// 	}
-
-// 	var isBig = $("#my_btn-font").text() == "A/a";
-// 	var isLight = $("#my_btn-bg").text() == "Light Theme";
-
-// 	$(".main").removeClass(!isLight ? "theme-light" : "theme-dark");
-// 	$(".main").addClass(isLight ? "theme-light" : "theme-dark");
-
-// 	$(".main").removeClass(!isBig ? "font-large" : "font-small");
-// 	$(".main").addClass(isBig ? "font-large" : "font-small");
-// }
-
-// 
-function themeSwitcherFonts(btnId) {
-	if (btnId == "#my_btn-font") {
+function themeSwitcherFonts() {
 		var isBig = $("#my_btn-font").text() == "A/a";
 		var newTitle = isBig ? 'a/A' : 'A/a';
 		$("#my_btn-font").html(newTitle);
-	} else {
-		throw "unexpected Id";
-}
 
-	var isBig = $("#my_btn-font").text() == "A/a";
+	isBig = $("#my_btn-font").text() == "A/a";
 
 	$(".main").removeClass(!isBig ? "font-large" : "font-small");
 	$(".main").addClass(isBig ? "font-large" : "font-small");
 }
 
-function themeSwitcherBg(btnId) {
-	if (btnId == "#my_btn-bg") {
-		var isLight = $("#my_btn-bg").text() != "Dark Theme";
-		var newTitle = isLight ? 'Dark Theme' : 'Light Theme';
+function themeSwitcherBg() {
+		var isDark = $("#my_btn-bg").text() != "Light Theme";
+		var newTitle = !isDark ? 'Dark Theme' : 'Light Theme';
 
 		$("#my_btn-bg").html(newTitle);
-	} else {
-		throw "unexpected Id";
-	}
 
-	var isLight = $("#my_btn-bg").text() == "Light Theme";
+	isDark = $("#my_btn-bg").text() != "Light Theme";
 
-	$(".main").removeClass(!isLight ? "theme-light" : "theme-dark");
-	$(".main").addClass(isLight ? "theme-light" : "theme-dark");
+	$(".main").removeClass(!isDark ? "theme-light" : "theme-dark");
+	$(".main").addClass(isDark ? "theme-light" : "theme-dark");
 
 }
 
@@ -67,51 +33,19 @@ function expandCollapseHandler() {
 }
 
 function initEventHandlers() {
-	$("#my_btn-font").on("click", function() {
-		themeSwitcherFonts("#my_btn-font");
-	});
-
-	$("#my_btn-bg").on("click", function() {
-		themeSwitcherBg("#my_btn-bg");
-	});
-
+	$("#my_btn-font").on("click", themeSwitcherFonts);
+	$("#my_btn-bg").on("click", themeSwitcherBg);
 	$(".move-arrow").on("click", expandCollapseHandler);
 
 	adjustExpanding();
 }
 
-$(document).ready(initEventHandlers);
+// $(document).ready(initEventHandlers);
 
-// window.onresize = function(event) {
-// 	adjustExpanding();
-// }
+$(document).on("ready",initEventHandlers);
+$(window).on("load", adjustExpanding);
+$(window).on("resize", adjustExpanding);
 
-// var oldExpandedState;
-$(window).on('load', function() {
-        adjustExpanding();
-    });
-
-$(window).on('resize', function() {
-    adjustExpanding();
-});
-// function adjustExpanding() {
-// 	var oldText = ["Version 1.0"];
-
-// 	if ($(window).width() < 1024) {
-// 		$(".aside-block").removeClass('show');
-
-// 		$(".app-version").innerHTML = oldText;
-// 		oldText.splice(0, 1, "V 1.0");
-// 		$(".app-version").html(oldText);
-
-// 	} else {
-// 		$(".aside-block").addClass('show');
-
-// 		$(".app-version").innerHTML = oldText;
-// 		oldText.splice(0, 1, "Version 1.0");
-// 		$(".app-version").html(oldText);
-// 	}
-// }
 
 function adjustExpanding (){
 	if ($(window).width () < 812) {
